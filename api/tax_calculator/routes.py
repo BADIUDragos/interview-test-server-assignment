de2e8ff6fb67d3b84fecdb13b2f8f5cd4f053c75
BADIUDragos <dragos.badiu.a@hotmail.com>
2024-04-21 16:15:35 -0400

    calculate tax endpoint plus tests

--- api/tax_calculator/routes.py
+++ api/tax_calculator/routes.py
@@ -1,24 +1,60 @@
-from flask import jsonify, render_template, redirect, request, abort
+from flask import jsonify, redirect, request, abort
 from app import app
 from . import controllers
-from .. import error_handlers
 from ..error_handlers import format_error
 
 
 @app.route('/tax-calculator/',  methods=['GET'])
 def tax_calculator_instructions():
+    for param in ['annual_income', 'tax_year']:
+        if request.args.get(param) is None:
+            error_response = format_error(
+                message=f"Please provide {param.replace('_', ' ')}",
+                field=param,
+                code=f"NO_{param.upper()}"
+            )
+            return jsonify({'errors': error_response}), 400
+
     try:
-        annual_income = float(request.args.get('salary'))
-    except TypeError as e:
-        pass
+        annual_income = float(request.args['annual_income'])
+    except ValueError:
+        error_response = format_error(
+            message="Annual_income must be a number",
+            field="annual_income",
+            code="INVALID_ANNUAL_INCOME"
+        )
+        return jsonify({'errors': error_response}), 400
+
+    try:
+        tax_year = int(request.args['tax_year'])
+    except ValueError:
+        error_response = format_error(
+            message="tax_year must be a valid year",
+            field="tax_year",
+            code="INVALID_TAX_YEAR"
+        )
+        return jsonify({'errors': error_response}), 400
+
+    if tax_year < 2019 or tax_year > 2023:
+        error_response = format_error(
+            message="tax_year must be between 2019 and 2023 inclusively",
+            field="tax_year",
+            code="TAX_YEAR_OUT_OF_RANGE"
+        )
+        return jsonify({'errors': error_response}), 400
+
+    try:
+        result = controllers.calculate_marginal_tax(annual_income, tax_year)
+    except Exception as e:
+        error_response = format_error(
+            message=str(e),
+        )
+        return jsonify({'errors': error_response}), 520
+
+    return jsonify(result)
 
 
-
-    return jsonify({
-        'tax_brackets': controllers.get_reliable_brackets()
-    })
-
-
+# odd way of doing things but ok
 @app.route('/tax-calculator/tax-year')
 def default_brackets():
     return redirect('/')
@@ -27,12 +63,13 @@ def default_brackets():
 @app.route('/tax-calculator/tax-brackets/<tax_year>')
 def tax_year_brackets(tax_year):
 
+    # would prefer not to write this in both endpoints but get the info from the same place
     years_supported = ['2019', '2020', '2021', '2022', '2023']
 
     if tax_year not in years_supported:
         error_response = format_error(
             message="We only support tax calculations for years: " + ", ".join(years_supported),
-            field="tax year",
+            field="tax_year",
             code="UNSUPPORTED_YEAR"
         )
 
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,15 +4,90 @@ from api.error_handlers import format_error
 from api.tax_calculator.tax_brackets import get_tax_brackets
 
 
+# I would split these into one class per route url as well
 @pytest.fixture
 def supported_years():
     return ['2019', '2020', '2021', '2022', '2023']
 
 
-def test_tax_calculator_instructions(client):
+def test_tax_calculator(client):
     resp = client.get('/tax-calculator/')
     brackets = get_tax_brackets(2022)
     assert resp.json == {'tax_brackets': brackets}
+
+
+def test_tax_calculator_missing_annual_income(client):
+    resp = client.get('/tax-calculator/')
+    assert resp.status_code == 400
+    assert resp.json == {
+        'errors': [{
+            'field': 'annual_income',
+            'message': 'Please provide annual income',
+            'code': 'NO_ANNUAL_INCOME'
+        }]
+    }
+
+
+def test_tax_calculator_missing_tax_year(client):
+    resp = client.get('/tax-calculator/', query_string={'annual_income': '50000'})
+    assert resp.status_code == 400
+    assert resp.json == {
+        'errors': [{
+            'field': 'tax_year',
+            'message': 'Please provide tax year',
+            'code': 'NO_TAX_YEAR'
+        }]
+    }
+
+
+def test_tax_calculator_bad_annual_income_type(client):
+    resp = client.get('/tax-calculator/', query_string={'annual_income': 'not a number', 'tax_year': 2021})
+    assert resp.status_code == 400
+    assert resp.json == {
+        'errors': [{
+            'field': 'annual_income',
+            'message': 'Annual_income must be a number',
+            'code': 'INVALID_ANNUAL_INCOME'
+        }]
+    }
+
+
+def test_tax_calculator_bad_tax_year(client):
+    resp = client.get('/tax-calculator/', query_string={'annual_income': '50000', 'tax_year': '2021.45'})
+    assert resp.status_code == 400
+    assert resp.json == {
+        'errors': [{
+            'field': 'tax_year',
+            'message': 'tax_year must be a valid year',
+            'code': 'INVALID_TAX_YEAR'
+        }]
+    }
+
+
+def test_tax_calculator_year_not_supported(client):
+    resp = client.get('/tax-calculator/', query_string={'annual_income': '50000', 'tax_year': '2025'})
+    assert resp.status_code == 400
+    assert resp.json == {
+        'errors': [{
+            'field': 'tax_year',
+            'message': 'tax_year must be between 2019 and 2023 inclusively',
+            'code': 'TAX_YEAR_OUT_OF_RANGE'
+        }]
+    }
+
+
+def test_tax_calculator_success(client):
+    resp = client.get('/tax-calculator/', query_string={'annual_income': '50000', 'tax_year': '2020'})
+    assert resp.status_code == 200
+    assert resp.json == {'effective_tax_rate': 0.1516,
+                         'taxes_owed_per_band': [[0.15, 7280.25], [0.205, 300.325]],
+                         'total_taxes': 7580.575}
+
+
+def test_tax_calculator_other_errors(client):
+    resp = client.get('/tax-calculator/', query_string={'annual_income': '4', 'tax_year': '2020'})
+    assert resp.status_code == 520
+    assert resp.json == {'errors': [{'code': '', 'field': '', 'message': "You're broke !"}]}
 
 
 def test_default_brackets(client):
@@ -22,7 +97,6 @@ def test_default_brackets(client):
 
 
 def test_tax_year_brackets(client, supported_years):
-
     for year in supported_years:
         resp = client.get(f'/tax-calculator/tax-brackets/{year}')
         brackets = get_tax_brackets(year)
@@ -32,10 +106,10 @@ def test_tax_year_brackets(client, supported_years):
 
 def test_tax_year_brackets_unsupported_year(client, supported_years):
     expected_result_data = format_error(
-            message="We only support tax calculations for years: " + ", ".join(supported_years),
-            field="tax year",
-            code="UNSUPPORTED_YEAR"
-        )
+        message="We only support tax calculations for years: " + ", ".join(supported_years),
+        field="tax_year",
+        code="UNSUPPORTED_YEAR"
+    )
 
     unsupported_year = '9999'
     resp = client.get(f'/tax-calculator/tax-brackets/{unsupported_year}')
